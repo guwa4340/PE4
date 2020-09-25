9c5c0eb88fcc8e3ce809d99d11a20b5e44684ce9
guwa4340 <gwallach98@gmail.com>
2020-09-25 22:42:37 +0000

    Player Choice and Testing

--- .ipynb_checkpoints/TicTacToe-checkpoint.cpp
+++ .ipynb_checkpoints/TicTacToe-checkpoint.cpp
@@ -4,27 +4,58 @@
 
 using namespace std;
 
-string * CreateBoard(){
-    string board [] = {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
+int **CreateBoard(){
+    //int l[9] = {0,1,2,3,4,5,6,7,8};
+    int** board = 0;
+    board = new int*[3];
+    for(int i = 0; i < 3; i++){
+        board[i] = new int[3];
+    }
+    board[0][0] = 0;
+    board[0][1] = 1;
+    board[0][2] = 2;
+    board[1][0] = 3;
+    board[1][1] = 4;
+    board[1][2] = 5;
+    board[2][0] = 6;
+    board[2][1] = 7;
+    board[2][2] = 8;
+    //cout << board << endl;
     return board;
 }
 
-void DisplayBoard(string * b){
-    int count = 0;
+void DisplayBoard(int ** b){
+    int count = 1;
     for(int i = 0;i<3;i++){
         for(int j = 0; j < 3; j++){
             if(count == 3){
-                cout << b[i][j] << endl;
-                count = 0;
+                cout << " " << b[i][j] << endl;
+                count = 1;
             }
             else{
-                cout << b[i][j];
+                cout << " " << b[i][j];
                 count = count + 1;
             }
         }
     }
 }
 
+string GetPlayerChoice(){
+    string choice;
+    cout << "Enter Your Choice As The Number Value" << endl;
+    cin >> choice;
+    return choice;
+}
+
+
 int main(){
+    //test create board function
+    int** board = CreateBoard();
+    //cout << "Past Create Board" << endl;
+    //test display board
+    DisplayBoard(board);
+    //cout << "Past Display Board" << endl;
+    //test player choice
+    GetPlayerChoice();
     return 0;
 }
