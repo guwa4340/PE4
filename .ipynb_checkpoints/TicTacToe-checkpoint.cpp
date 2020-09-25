5f7f92d927914ad47369c622138f18d4053878c1
guwa4340 <gwallach98@gmail.com>
2020-09-25 20:59:40 +0000

    Added Makefile and DisplayBoard

--- .ipynb_checkpoints/TicTacToe-checkpoint.cpp
+++ .ipynb_checkpoints/TicTacToe-checkpoint.cpp
@@ -5,8 +5,26 @@
 using namespace std;
 
 string * CreateBoard(){
-    string board [] = {{'','',''},{'','',''},{'','',''}};
+    string board [] = {{' ',' ',' '},{' ',' ',' '},{' ',' ',' '}};
     return board;
 }
 
+void DisplayBoard(string * b){
+    int count = 0;
+    for(int i = 0;i<3;i++){
+        for(int j = 0; j < 3; j++){
+            if(count == 3){
+                cout << b[i][j] << endl;
+                count = 0;
+            }
+            else{
+                cout << b[i][j];
+                count = count + 1;
+            }
+        }
+    }
+}
 
+int main(){
+    return 0;
+}
